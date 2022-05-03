7d8e5dba7e789a035c823569cfccde40f4a6206b
Euphoria99 <363pavanbhat@gmail.com>
2022-05-03 11:43:55 +0530

    Updating Existing Notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myfirstnotes/constants/routes.dart';
 import 'package:myfirstnotes/services/auth/auth_service.dart';
 import 'package:myfirstnotes/views/login_view.dart';
-import 'package:myfirstnotes/views/notes/new_notes_view.dart';
+import 'package:myfirstnotes/views/notes/create_update_note_view.dart';
 import 'package:myfirstnotes/views/notes/notes_view.dart';
 import 'package:myfirstnotes/views/register_view.dart';
 import 'package:myfirstnotes/views/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoutes: ((context) => const NotesView()),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
